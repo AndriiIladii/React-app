5f8bfd36b1dec6edf9d8ed05c91d1aa84a812f88
AndriiIladii <iladiy21@gmail.com>
2024-04-16 11:59:26 +0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
-import React, { useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useId,
+  useMemo,
+  useCallback,
+  useReducer,
+} from "react";
 import List from "./components/List";
 import * as styles from "./App.module.css";
 
 const App = () => {
   const [list, setList] = useState([0, 1, 2, 3]);
+  const [counter, setCounter] = useState(0);
+  const id = useId();
+  const ref = useRef(0);
+  const load = useCallback(
+    (a) => {
+      for (let i = 0; i < 1000000 * a; i++) {
+        setList(i);
+      }
+    },
+    [a]
+  );
+
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      if (action.type === "change_name") {
+        return {
+          name: action.payload,
+        };
+      }
+    },
+    { name: "Andrii" }
+  );
+
+  useEffect(() => {
+    dispatch({ type: "change_name", payload: "John" });
+  }, []);
+
+  useEffect(() => {}, [ref]);
 
   return (
-    <List text="text">
+    <List text="text" load={load}>
+      <div className="" ref={ref}>
+        {ref.current}
+      </div>
       {list.map((item, index) => {
         return (
           <div key={item} className={styles.test}>
